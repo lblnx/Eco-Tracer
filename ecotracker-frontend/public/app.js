109bfef12d0856fb6d8662caa373004ebafc94b1
lblnx <pabloquint.2002@gmail.com>
2026-05-12 18:37:06 -0600

    html y js y css

---
+++ ecotracker-frontend/public/app.js
@@ -0,0 +1,25 @@
+const BACKEND_URL = "http://localhost:8000"; 
+
+async function testAPI() {
+    const resDiv = document.getElementById('resultado');
+    resDiv.innerText = "Cargando...";
+    try {
+        const response = await fetch(BACKEND_URL);
+        const data = await response.json();
+        resDiv.innerText = JSON.stringify(data, null, 2);
+    } catch (error) {
+        resDiv.innerText = "Error conectando al backend.";
+    }
+}
+
+async function testError() {
+    const resDiv = document.getElementById('resultado');
+    resDiv.innerText = "Generando error...";
+    try {
+        const response = await fetch(BACKEND_URL, { method: "POST" });
+        const data = await response.json();
+        resDiv.innerText = JSON.stringify(data, null, 2);
+    } catch (error) {
+        resDiv.innerText = "Error capturado.";
+    }
+}
